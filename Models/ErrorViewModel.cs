2911cda3f0fb8bf4aecb036665e424bcfa385cda
Ankur Apte <ankurapte@outlook.com>
2024-07-24 01:28:34 +0530

    Initial Commit for the EffortCalculator App

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace EffortCalculator.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
