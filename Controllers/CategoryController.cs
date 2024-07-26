beba06a5c92ad587b061d088cb0b58304e143336
Ankur Apte <ankurapte@outlook.com>
2024-07-27 01:54:19 +0530

    Added CSS Added icon library as CDN Rendered the Category view

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -4,9 +4,15 @@ namespace EffortCalculator.Controllers;
 
 public class CategoryController : Controller
 {
+    readonly ApplicationDbContext _db;
+    public CategoryController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
     // GET: CategoryController
     public ActionResult Index()
     {
-        return View();
+        List<Category> categories = _db.Categories.ToList();
+        return View(categories);
     }
 }
