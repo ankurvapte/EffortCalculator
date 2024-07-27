c8431b6d61f1ae3406f61ca8681419197f0539b5
Ankur Apte <ankurapte@outlook.com>
2024-07-28 01:22:59 +0530

    Added edit functionality for categories

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -35,7 +35,37 @@ public class CategoryController : Controller
         {
             _db.Categories.Add(category);
             _db.SaveChanges();
-            return RedirectToAction("Index", category);
+            return RedirectToAction("Index");
+        }
+
+        return View();
+    }
+
+    public ActionResult Edit(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+        var category = _db.Categories.Where(t => t.Id == id).FirstOrDefault();
+        return View(category);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Category category)
+    {
+        if (category.Name != null &&
+            (category.Name.ToLower() == "admin" ||
+             category.Name.ToLower().StartsWith("test")))
+        {
+            ModelState.AddModelError("Name", "Please do not start any of the categories with the name test. And yes, admin is reserved too.");
+        }
+
+        if (ModelState.IsValid)
+        {
+            _db.Categories.Update(category);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
 
         return View();
