5162240659204c650c88d878da0386ce9654f22e
Ankur Apte <ankurapte@outlook.com>
2024-07-28 00:45:59 +0530

    Added view and methods in view to display the form and add new category in the database. The model is now changed to implement validations. Included a partial view to run jQuery validations on the client side.

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -15,4 +15,29 @@ public class CategoryController : Controller
         List<Category> categories = _db.Categories.ToList();
         return View(categories);
     }
+
+    public ActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Category category)
+    {
+        if (category.Name != null &&
+            (category.Name.ToLower() == "admin" ||
+             category.Name.ToLower().StartsWith("test")))
+        {
+            ModelState.AddModelError("Name", "Please do not start any of the categories with the name test. And yes, admin is reserved too.");
+        }
+
+        if (ModelState.IsValid)
+        {
+            _db.Categories.Add(category);
+            _db.SaveChanges();
+            return RedirectToAction("Index", category);
+        }
+
+        return View();
+    }
 }
--- Models/Category.cs
+++ Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace EffortCalculator;
 
@@ -7,7 +8,11 @@ public class Category
     [Key]
     public int Id { get; set; }
     [Required]
+    [DisplayName("Category Name")]
+    [MaxLength(30)]
     public required string Name { get; set; }
 
+    [DisplayName("Display Order")]
+    [Range(1, 100)]
     public int DisplayOrder { get; set; }
 }
